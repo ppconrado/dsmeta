ca9b3fd72a4743812d8b5ddce7c131f0e6fa240d
ppconrado <ppconrado@yahoo.com.br>
2022-07-12 04:35:03 -0300

    Notification botton component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import NotificationButton from "./components/NotificationButton/Index";
+
 function App() {
-  return <h1>ppconrado</h1>;
+  return (
+    <>
+      <h1>ppconrado</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
 export default App;
